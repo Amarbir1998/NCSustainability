5c7b9ca82390d0087319e88faee6d6bb922dfb4f
Amarbir Singh <57962702+Amarbir1998@users.noreply.github.com>
2020-01-30 17:43:57 -0500

    Changed by Amarbir

---
+++ NC Sustainability/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NC_Sustainability.Models
+{
+    public class Event
+    {
+    }
+}
